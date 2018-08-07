67bbe6b18e0471f515e9bf04014cd84d93b97a9e
Adam Forestier <aforestier10@gmail.com>
2018-08-06 18:05:02 -0700

    Find all white space in a string, and replace it

---
+++ ReplaceWhiteSpace/ReplaceWhiteSpace/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/// <summary>
+/// Method to replace white space in a string and return new string
+/// Much like a URL does
+/// </summary>
+
+namespace ReplaceWhiteSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // test input
+            string url = "www. google .com";
+
+            // Call method, pass in ar
+            Console.WriteLine("The original string {0} has been transformed to {1}", url, RemoveSpace(url));
+
+            // keep console open
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Use regular expressions to find all white space 
+        /// Replace white space with %20
+        /// Return the new string
+        /// </summary>
+        /// <param name="str"></param>
+        private static string RemoveSpace(string str)
+        {
+            string newString = System.Text.RegularExpressions.Regex.Replace(str, @"\s", "%20");
+            return newString;
+        }
+    }
+}
