9abafa189b5e742662f9f5e1a53f9774a21f69cc
Adam Forestier <aforestier10@gmail.com>
2018-09-05 21:04:23 -0500

    For number n, find all prime numbers less than n

---
+++ PrimeNums/PrimeNums/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+/// <summary>
+/// Print all prime numbers less than n
+/// </summary>
+
+namespace PrimeNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testNumber = 20;
+
+            Console.WriteLine("Prime numbers less than n:");
+            PrimeNums(testNumber);
+
+            Console.Read();
+        }
+
+        private static void PrimeNums(int n)
+        {
+            bool[] primes = new bool[n + 1];
+
+            for (int i = 0; i < n; i++)
+                primes[i] = true;
+
+            for (int i = 2; i*i <= n; i++)
+            {
+                if (primes[i] == true)
+                {
+                    for (int p = i*2; p <= n; p += i)
+                    {
+                        primes[p] = false;
+                    }
+                }
+            }
+
+            for (int i = 2; i < n; i++)
+            {
+                if (primes[i] == true)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
